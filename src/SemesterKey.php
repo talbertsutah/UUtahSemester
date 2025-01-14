9056c21ad81df19b19dbe7e557fc0a2dcc49e61b
Tom Alberts <59806885+talbertsutah@users.noreply.github.com>
2025-01-13 17:56:07 -0700

    Create SemesterKey.php

---
+++ src/SemesterKey.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Helpers;
+use App\Traits\EnumToArray;
+
+/*  Enum that holds information about the scheme for converting semesters to codes
+*
+*   The scheme from string to integer representation is 'SEMESTER YEAR' -> XXXY, where XXX = YEAR - 1900, and SEMESTER is represented by the digit Y
+*   
+*   SEMESTER is one of {Spring, Summer, Fall}, which map to Spring->4, Summer->6, Fall->8
+*
+*   ZeroYear = 1900 stores the fact that 1900 is the minimum year
+*/
+
+// ?? TODO: Put this key directly into the Semester class? Can that be done? Is it advisable?
+
+enum SemesterKey: int 
+{
+    use EnumToArray;
+
+    case Spring = 4;
+    case Summer = 6;
+    case Fall = 8;
+
+}
